ee08b062bcb223b38bbe0c73af4c12a60c92d2b2
Tamer-Alwarei <31402805+Tamer-Alwarei@users.noreply.github.com>
2021-02-21 22:18:02 +0100

    First commit

---
+++ jsmvc/v1/js/js.js
@@ -0,0 +1,25 @@
+
+var priceType1 = 50;
+var priceType2 = 80;
+var priceType3 = 30;
+
+var quantity1 = 30;
+var quantity2 = 20;
+var quantity3 = 46;
+ 
+var alltyps = ["shawerma","burger","chips"];
+var allprice = [50,80,30];
+var allquantity = [30,20,46];
+
+
+
+for (var i=0; i < 3 ; i++) {
+    console.log(allprice[i] * allquantity[i])
+}
+var totalprice;
+
+// totalprice = ( priceType1 * quantity1 ) + ( priceType2 * quantity2 ) + ( priceType3 * quantity3 )
+
+
+
+// console.log(totalprice)
